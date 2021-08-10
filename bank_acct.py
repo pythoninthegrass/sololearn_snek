f5c8be7b5fa3b6ffd99b86cbb5e5a0685304b9ff
Lance Stephens <4097471+pythoninthegrass@users.noreply.github.com>
2021-08-10 11:14:55 -0600

    Solved data hiding exercise

--- bank_acct.py
+++ bank_acct.py
@@ -1,14 +1,16 @@
 class BankAccount:
     def __init__(self, balance):
         self._balance = balance
+        print(f"Current balance: {self._balance}")
+
+    def deposit(self, value):
+        self._value = value
+        return value + self._balance
 
     def __repr__(self):
-         return "Account Balance: {}".format(self._balance)
-
-    def deposit(self, amount):
-        #your code goes here
-        self._amount = amount
+         return f"Account Balance: {self._value}"
 
 acc = BankAccount(0)
+print("How much to deposit?")
 acc.deposit(int(input()))
 print(acc)
