907502d3137dab9945c553ff7833155a8861d622
Lance Stephens <4097471+pythoninthegrass@users.noreply.github.com>
2021-08-17 20:08:20 -0500

    83.3 regex
    Hidden test case #5 is a blocker

---
+++ re_do_better.py
@@ -0,0 +1,8 @@
+import re
+
+quote = "Always do your best. Your best is going to change from moment to moment; it will be different when you are healthy as opposed to sick. Under any circumstance, simply do your best, and you will avoid self-judgment, self-abuse and regret"
+
+word = input()
+flags = re.IGNORECASE
+result = re.findall(word, quote, flags)
+print(len(result))
