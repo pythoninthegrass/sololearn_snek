9882e02085475407db7591737c9c8baaab12b34f
Lance Stephens <4097471+pythoninthegrass@users.noreply.github.com>
2021-08-18 17:06:42 -0500

    case matters
    So much for stretch goals

--- re_do_better.py
+++ re_do_better.py
@@ -3,6 +3,7 @@ import re
 quote = "Always do your best. Your best is going to change from moment to moment; it will be different when you are healthy as opposed to sick. Under any circumstance, simply do your best, and you will avoid self-judgment, self-abuse and regret"
 
 word = input()
-flags = re.IGNORECASE
-result = re.findall(word, quote, flags)
+# flags = re.IGNORECASE
+# result = re.findall(word, quote, flags)
+result = re.findall(word, quote)
 print(len(result))
