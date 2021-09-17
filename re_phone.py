e3484926868c60c77aab2b8077a8fae5b162129a
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2021-09-16 22:56:36 -0500

    91 phone number validator - needed anchor EOL

--- re_phone.py
+++ re_phone.py
@@ -1,18 +1,12 @@
 import re
 
-# valid phone number has exactly 8 digits and starts with 1, 8 or 9. 
-# e.g., 81234567, 57345672
+# valid phone number has exactly 8 digits and starts with 1, 8 or 9.
+# e.g., 81234567, 57345672, 98765432
 
-#num_regex = re.compile(r'((\d\d\d-)?\d\d\d-\d\d\d\d(,)?(.)?|(.)+){3}')
-#print(num_regex.search("My numbers are 415-555-1234, 555-4242, and 212-555-0000"))
+num = input()
 
-# num = input()
-num = '57345672'
+num_regex = re.compile(r'^[1|8|9]\d{1,8}$')
 
-# TODO: QA edge case test that fails
-num_regex = re.compile(r'^(1|8|9)(\d){0,8}')
-# num_regex.search("57345672")
-print(num_regex.match(num))
 if num_regex.match(num):
 	print('Valid')
 else:
