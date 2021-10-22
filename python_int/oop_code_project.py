6d77f7b5eae9e9d74daf4085cfc7a4123b6cb20d
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2021-10-22 00:57:18 -0500

    Update oop_code_project.py
    A wild dundee appears
    
    Co-Authored-By: Elena Ackley <62074891+elenaelago@users.noreply.github.com>

--- python_int/oop_code_project.py
+++ python_int/oop_code_project.py
@@ -20,16 +20,25 @@ class Alien(Enemy):
   def __init__(self):
     super().__init__('Alien', 5)
 
+class OwnWorst(Enemy):
+  def __init__(self):
+    super().__init__('Human', 2)
+
 
 m = Monster()
 a = Alien()
+o = OwnWorst()
 
-# TODO: error handling for when enemy is dead
-while True:
-    x = input('Raise your weapon: ')
+
+while (m.lives > 0 or a.lives > 0) and o.lives > 0:
+    x = input('gun or laser?\nRaise your weapon: ')
     if x == 'exit':
+        print('The only way to win is to not start.')
         break
     elif x == 'laser':
         a.hit()
     elif x == 'gun':
         m.hit()
+    else:
+        print(r"That's not a knife. THIS is a knife.")
+        o.hit()
