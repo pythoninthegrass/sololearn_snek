b128ed39d7252821b69b688dc4445313ded056db
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2021-10-24 22:34:19 -0500

    30.2 finally, else - try/except w/isnumeric and isalpha

--- python_int/finally.py
+++ python_int/finally.py
@@ -1,9 +1,17 @@
 menu = ['Fries', 'Sandwich', 'Cheeseburger', 'Coffee', 'Soda']
 
-# TODO: 3, item not found
+item = input()
+
 try:
-	for i in range(menu):
-		if input() == i:
-			print(f"{i}\nThanks for your order")
+    if item.isnumeric():
+        item = int(item)
+        for i in range(len(menu)):
+            if item == i:
+                print(f"{menu[i]}\nThanks for your order")
+                break
+            elif item > len(menu):
+                raise IndexError
+    elif item.isalpha():
+        raise TypeError
 except (IndexError, TypeError):
-	print('Item not found')
+    print('Item not found')
